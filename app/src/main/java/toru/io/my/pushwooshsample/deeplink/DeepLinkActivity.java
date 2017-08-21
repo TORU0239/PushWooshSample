8cf8eef35dbfcaad8dcc47c1ed2cb33d1bd05f72
Toru Choi <toru.choi@11street.my>
2017-08-21 15:38:40 +0800

    divide into package

---
+++ app/src/main/java/toru/io/my/pushwooshsample/deeplink/DeepLinkActivity.java
@@ -0,0 +1,15 @@
+package toru.io.my.pushwooshsample.deeplink;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import toru.io.my.pushwooshsample.R;
+
+public class DeepLinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deep_link);
+    }
+}
--- app/src/main/java/toru/io/my/pushwooshsample/main/MainActivity.java
+++ app/src/main/java/toru/io/my/pushwooshsample/main/MainActivity.java
@@ -1,9 +1,11 @@
-package toru.io.my.pushwooshsample;
+package toru.io.my.pushwooshsample.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.pushwoosh.Pushwoosh;
+
+import toru.io.my.pushwooshsample.R;
 
 public class MainActivity extends AppCompatActivity {
 
