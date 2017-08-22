9f7f1000cee92c2cd3d78536f8878dcab6c4d6a6
Toru Choi <toru.choi@11street.my>
2017-08-22 11:31:05 +0800

    databinding applied to user info model

--- app/src/main/java/toru/io/my/pushwooshsample/info/model/UserInfo.java
+++ app/src/main/java/toru/io/my/pushwooshsample/info/model/UserInfo.java
@@ -1,35 +1,48 @@
 package toru.io.my.pushwooshsample.info.model;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import toru.io.my.pushwooshsample.BR;
+
 /**
  * Created by toruchoi on 22/08/2017.
  */
 
-public class UserInfo {
+public class UserInfo extends BaseObservable{
     private String userName;
     private String userGender;
     private String userAge;
 
+    public UserInfo() {}
+
+    @Bindable
     public String getUserName() {
         return userName;
     }
 
     public void setUserName(String userName) {
         this.userName = userName;
+        notifyPropertyChanged(BR.userName);
     }
 
+    @Bindable
     public String getUserGender() {
         return userGender;
     }
 
     public void setUserGender(String userGender) {
         this.userGender = userGender;
+        notifyPropertyChanged(BR.userGender);
     }
 
+    @Bindable
     public String getUserAge() {
         return userAge;
     }
 
     public void setUserAge(String userAge) {
         this.userAge = userAge;
+        notifyPropertyChanged(BR.userAge);
     }
 }
